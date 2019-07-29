1a2d12c2b1ba3d78c8bd485c9e5f05fe00b44ef3
Kshitij Purwar <kshitij.purwar4@gmail.com>
2019-07-30 01:26:21 +0530

    some done

--- ex8/ex8.js
+++ ex8/ex8.js
@@ -1,10 +1,26 @@
-$(document).ready(function(){
-	var $btn = $("#btn"),
-		$list = $("#list");
+$(document).ready(function() {
+  var $btn = $("#btn"),
+    $list = $("#list");
 
-	$btn.click(function(evt){
-		// TODO
-	});
+  var rsq = ASQ.react.of();
+  var rsq_sampled = ASQ.react.of();
 
-	// TODO: setup sampled sequence, populate $list
+  $btn.click(function(evt) {
+    rsq.push(evt);
+    // TODO
+  });
+
+
+  setInterval(function(){
+    if(){
+
+    }
+    rsq_sampled.push(rsq)
+  }, 1000)
+  rsq_sampled.react.throttle(rsq, 1000);
+
+  // TODO: setup sampled sequence, populate $list
+  rsq_sampled.val(() => {
+    $list.append("clicked");
+  });
 });
